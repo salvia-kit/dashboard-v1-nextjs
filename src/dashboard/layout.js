c59adda5811e3a0627c641184a0b7a65ec301b8e
enochndika <enochbeloved@gmail.com>
2021-07-02 19:58:54 +0100

    change index.js by layout.js in dashboard folder

--- src/dashboard/layout.js
+++ src/dashboard/layout.js
@@ -7,7 +7,7 @@ import DashboardProvider from './provider/context';
 const style = {
   content: `h-screen overflow-hidden relative lg:rounded-2xl`,
   main: `h-screen overflow-auto pb-36 pt-4 px-2 md:pb-8 md:px-4 lg:px-6`,
-  mainContainer: `flex flex-col h-screen pl-0 w-full lg:space-y-4 lg:w-99`,
+  mainContainer: `flex flex-col h-screen pl-0 w-full lg:w-99 lg:space-y-4`,
   container: `bg-mac bg-center bg-cover h-screen overflow-hidden w-full lg:p-4`,
 };
 
