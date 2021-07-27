6ce4954e3087bafed51a5a728485abc3b05eb3f4
enochndika <enochbeloved@gmail.com>
2021-07-28 00:12:48 +0100

    add initial value for useRef, compare state value with previous value && update title

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>
-          {process.env.NODE_ENV === 'development'
-            ? 'Salvia-kit Dashboard v1 Next.js'
-            : 'Salvia-kit Dashboard v1 Demo'}
-        </title>
+        <title>Salvia-kit Dashboard v1 Next.js</title>
       </Head>
       <DashboardLayout>
         <Component {...pageProps} />
